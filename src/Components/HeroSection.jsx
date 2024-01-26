b25fcf8c0e10d096552ded6b9802df2c478971f1
habibur18 <amarhabibur@gmail.com>
2024-01-26 19:15:45 +0600

    make herosection align

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeroSection() {
   return (
     <section className="pb-[114px] pt-20 md:mt-[100px]">
-      <div className="container lg:px-20">
+      <div className="container mx-auto lg:px-20">
         <div className="grid items-center gap-6 md:grid-cols-2">
           <div className="flex justify-center md:order-2">
             <img className="max-md:w-full" src="https://i.ibb.co/6nVq5Dt/frame.png" width="326" height="290" alt="frame" />
